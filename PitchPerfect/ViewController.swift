f9c28c3e807bd76b7bb80d115bbeb773e999584d
Ilqar Ilyasov <imilqar@gmail.com>
2018-09-07 07:47:51 -0500

    Created all IBOutlet and IBAction. Images added to Assets

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -11,12 +11,23 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear called")
+    }
+    
+//    override func viewDidAppear(_ animated: Bool) {
+//        super.viewDidAppear(animated)
+//    }
+//
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
